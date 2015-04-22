8d4d07deac3802f44644a281e1960517fb39d512
Steve Nester <steve@sahfor.com>
2015-04-22 02:22:05 -0700

    backoffice

---
+++ backoffice-CAPTURE.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import urllib, urllib2, json
+
+def backofficeOperation():
+	url = "https://test.oppwa.com/v1/payments/8a82944a4cdca5cc014ce06c9216480a"
+	data = {
+		'authentication.userId' : '8a8294174b7ecb28014b9699220015cc',
+		'authentication.password' : 'sy6KJsT8',
+		'authentication.entityId' : '8a8294174b7ecb28014b9699a3cf15d1',
+		'amount' : '10.00',
+		'currency' : 'EUR',
+		'paymentType' : 'CP'
+	}
+	try:
+		response = urllib2.urlopen(url, urllib.urlencode(data))
+		return json.loads(response.read());
+	except urllib2.HTTPError, e:
+		return e.code;
+
+responseData = backofficeOperation();
+print responseData;
---
+++ backoffice-REVERSAL.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import urllib, urllib2, json
+
+def backofficeOperation():
+	url = "https://test.oppwa.com/v1/payments/8a82944a4cdca5cc014ce06ccafe481d"
+	data = {
+		'authentication.userId' : '8a8294174b7ecb28014b9699220015cc',
+		'authentication.password' : 'sy6KJsT8',
+		'authentication.entityId' : '8a8294174b7ecb28014b9699a3cf15d1',
+		'paymentType' : 'RV'
+	}
+	try:
+		response = urllib2.urlopen(url, urllib.urlencode(data))
+		return json.loads(response.read());
+	except urllib2.HTTPError, e:
+		return e.code;
+
+responseData = backofficeOperation();
+print responseData;
